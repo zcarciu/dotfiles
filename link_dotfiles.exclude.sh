79b7d906cfc9fbd0faa7b2903133ebc3024e10c8
Zachary Carciu <zcarciu@gmail.com>
2019-07-14 03:12:11 +0000

    removed unnecessary script

--- link_dotfiles.exclude.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# taken from here:
-# https://www.freecodecamp.org/news/dive-into-dotfiles-part-2-6321b4a73608/
-
-for file in $(ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md|\.swp$')
-do
-    ln -sv "$PWD/$file" "$HOME"
-done
